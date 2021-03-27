d2c9b581c3cc448a5bc2b2c64607147f3b568a5a
Jorge <jepers2021@gmail.com>
2021-03-26 21:49:55 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A133590.Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ejercicio 4.");
+            Console.WriteLine("Ingrese frases separadas con la tecla enter.");
+            Console.WriteLine("El programa finalizará cuando ingrese \"fin\"");
+
+            while (Console.ReadLine() != "fin") ;
+
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
